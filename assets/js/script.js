60d5e552382ac9e9cbcaebd3c29eb395cd6298f3
Ali <you@example.com>
2023-06-13 14:58:49 +0330

    header second done

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,11 @@ setInterval(() => {
   let date = new persianDate();
   dateElem.append(date.toLocale("fa").format("dddd DD MMMM YYYY hh:mm:ss"));
 }, 1000);
+
+
+
+
+// const swiper = new Swiper('.swiper', {
+//   freeMode: true,
+//   slidesPerView: '  ',
+// });
